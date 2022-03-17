e242394451fd80c29c881e034e58b4a785ea6ea2
Ian <99413314+IanAAB@users.noreply.github.com>
2022-03-17 09:38:08 -0600

    Add files via upload

---
+++ questao1.js
@@ -0,0 +1,35 @@
+let aluno1 = 'Jony'
+let aluno2 = 'Keller'
+let aluno3 = 'Daniel'
+let aluno4 = 'Ian'
+let aluno5 = 'Murilo'
+console.log('Grupo: ' + aluno1 + ', ' + aluno2 + ', ' + aluno3 + ', ' + aluno4 + ', ' + aluno5 + '.' )
+ 
+let valor1 = 593
+let valor2 = 4508
+let valor3 = 4085
+let valor4 = 1408
+let valor5 = 9032
+console.log('Valores: ' + valor1 + ', ' + valor2 + ', ' + valor3 + ', ' + valor4 + ', ' + valor5 + '.')
+ 
+let Questao_1A_Feita = true
+let Questao_1B_Feita = true
+let Questao_1C_Feita = false
+let Mundial_do_Palmeiras = false
+let Champions_do_City = false
+console.log('Booleans: ' + Questao_1A_Feita + ', ' + Questao_1B_Feita + ', ' + Questao_1C_Feita + ', ' + Mundial_do_Palmeiras + ', ' + Champions_do_City + '.')
+ 
+let salarioAndre = 4100
+let salarioJulio = 3750
+let salarioMarcos = 2900
+let salarioFernando = 1950
+let salarioCarlos = 1870
+console.log('Salários (Ordem Decrescente): ' + salarioAndre + ', ' + salarioJulio + ', ' + salarioMarcos + ', ' + salarioFernando + ', ' + salarioCarlos + '.')
+ 
+const inteiro1 = 3180
+const naoInteiro1 = 34.7
+const texto1 = '17 de março de 2022'
+const boolean1 = true
+const boolean2 = false
+console.log('Inteiro: ' + inteiro1 + '. ' + 'Não Inteiro: '+ naoInteiro1 + '. ' + 'Texto: ' + texto1 + '. ' + 'Booleanos: ' + boolean1 + ', ' + boolean2 + '.')
+
